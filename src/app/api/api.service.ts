45b3468bdce7c293692027733da3c95f11898dec
MYZ527 <maofengmi761@gmail.com>
2023-05-12 17:36:26 +0800

    purchase

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -5,42 +5,35 @@ import {Observable} from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiService {
 
-  private BaseUrl: string = 'https://jsonplaceholder.typicode.com';
+  private BaseUrl: string = 'http://localhost:8080/authority/v1.0';
   constructor(private http: HttpClient) { }
-
-  //get
-  // getAllRequest(): Observable<any> {
-  //   //https://jsonplaceholder.typicode.com/posts
-  //   const url = `${this.BaseUrl}/posts`;
-  //   return this.http.get(url);
-  // }
-  //加上回傳值page並設定limit為10
 
 //getAll
 getAllRequest(page: number): Observable<any> {
-  const url = `${this.BaseUrl}/posts?_page=${page}&_limit=10`;
+  const url = this.BaseUrl +'/purchases'+ '?page=' + page + '&limit=20';
   return this.http.get(url);
 }
 // getOne
 getOneRequest(id: any): Observable<any> {
-  const url = `${this.BaseUrl}/posts/${id}`;
+  const url = `${this.BaseUrl}/purchases/${id}`;
   return this.http.get(url);
 }
 //post
 postRequest(body: any): Observable<any> {
-  const url = `${this.BaseUrl}/posts`;
+  const url = `${this.BaseUrl}/purchases`;
   return this.http.post(url, body);
 }
 // patch
 patchRequest(id: any, body: any): Observable<any> {
-  const url = `${this.BaseUrl}/posts/${id}`;
+  const url = `${this.BaseUrl}/purchases/${id}`;
   return this.http.patch(url, body);
 }
 // delete
 deleteRequest(id: any): Observable<any> {
-  const url = `${this.BaseUrl}/posts/${id}`;
+  const url = `${this.BaseUrl}/purchases/${id}`;
   return this.http.delete(url);
 }
 
--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -5,15 +5,17 @@ import { LazyLoadEvent } from 'primeng/api';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 
+//定義
 interface purchase {
-  name: string; //申請人
-  position: string; //請購編號
-  company: string; //公司名稱
-  department: string; //請購部門
+  applicant: string; //申請人
+  purchase_id: string; //請購編號
+  company_name: string; //公司名稱
+  requisition_department: string; //請購部門
   product: string; //品名
-  quantity: number; //請購數量
+  requisition_quantity: number; //請購數量
   price: number; //單價
-  creater: string ; //創建者
+  position:string ; //流水號
+  created_by: string ; //創建者
 }
 
 @Component({
@@ -21,8 +23,9 @@ interface purchase {
   templateUrl: './purchase.component.html',
   styleUrls: ['./purchase.component.scss'],
 })
+
 export class PurchaseComponent {
-  purchase: purchase[] = [];
+  purchases: purchase[] = [];
   visible: boolean = false;
   form: FormGroup;
   addData: boolean = false;
@@ -33,14 +36,14 @@ export class PurchaseComponent {
     private route: ActivatedRoute,
   ) {
     this.form = this.fb.group({
-      name: ['', [Validators.required]], //必填
-      position: [''],
-      company: ['', [Validators.required]],
-      department: ['', [Validators.required]],
+      applicant: ['', [Validators.required]], //必填
+      position: ['',],
+      company_name: ['', [Validators.required]],
+      requisition_department: ['', [Validators.required]],
       product: ['', [Validators.required]],
-      quantity: ['', [Validators.required]],
+      requisition_quantity: ['', [Validators.required]],
       price: ['', [Validators.required]],
-      creater: ['', [Validators.required]],
+      created_by: ['', [Validators.required]],
     });
   }
 
@@ -48,13 +51,14 @@ export class PurchaseComponent {
   apiData!: ApiService[];
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
-    console.log(this.id);
+    this.getAll();
+    // this.id = this.route.snapshot.paramMap.get('id');
+    // console.log(this.id);
 
-    this.purchase = [
-      { name: 'pen', position: 'P-1', company: '這是筆', department: '業務部', product:'桌子', quantity: 2, price:8000 , creater: '王曉明' },
-      { name: 'apple', position: 'P-2', company: '這是蘋果', department: '技術部', product:'', quantity: 2, price: 8000, creater: ''  },
-    ];
+    // this.purchase = [
+    //   { name: 'pen', purchase_id: 'P-1', company: '這是筆', department: '業務部', product:'桌子', quantity: 2, price:8000 , creater: '王曉明' },
+    //   { name: 'apple', purchase_id: 'P-2', company: '這是蘋果', depaapplicantrtment: '技術部', product:'', quantity: 0, price: 0, creater: ''  },
+    // ];
   }
 
   // 懶加載
@@ -69,15 +73,16 @@ export class PurchaseComponent {
   //新增
   post(): void {
     let body = {
-      name: this.form.controls['name'].value,
-      position: this.form.controls['position'].value,
-      company: this.form.controls['company'].value,
-      department: this.form.controls['department'].value,
+      applicant: this.form.controls['applicant'].value,
+      purchase_id: this.form.controls['purchase_id'].value,
+      company_name: this.form.controls['company_name'].value,
+      requisition_department: this.form.controls['requisition_department'].value,
       product: this.form.controls['product'].value,
-      quantity: this.form.controls['quantity'].value,
-      price: this.form.controls['price'].value,
-      creater: this.form.controls['creater'].value,
+      requisition_quantity: Number(this.form.controls['requisition_quantity'].value),
+      price: Number(this.form.controls['price'].value),
+      created_by: this.form.controls['created_by'].value,
     };
+    console.log(body)
     this.HttpApi.postRequest(body).subscribe((request) => {
       console.log(request);
       // 新增成功後的動作
@@ -86,34 +91,35 @@ export class PurchaseComponent {
         icon: 'success',
         title: '成功',
       });
-      this.data.push(body);
+      this.getAll()
     });
   }
 
   //讀取
-  getOne(id: any): void {
-    this.HttpApi.getOneRequest(id).subscribe((data) => {
-      console.log(data);
-      this.form.patchValue(data);
+  getAll(): void {
+    this.HttpApi.getAllRequest(1).subscribe((data) => {
+      this.apiData = data.body.purchase;
+      console.log(this.apiData);
     });
   }
 
   //更新
-  patch(id: any): void {
+  patch(id : any): void {
     let body = {
-      name: this.form.controls['name'].value,
-      position: this.form.controls['position'].value,
-      company: this.form.controls['company'].value,
-      department: this.form.controls['department'].value,
+      applicant: this.form.controls['applicant'].value,
+      //purchase_id: this.form.controls['purchase_id'].value,
+      company_name: this.form.controls['company_name'].value,
+      requisition_department: this.form.controls['requisition_department'].value,
       product: this.form.controls['product'].value,
-      quantity: this.form.controls['quantity'].value,
-      price: this.form.controls['price'].value,
-      creater: this.form.controls['creater'].value,
+      requisition_quantity: Number(this.form.controls['requisition_quantity'].value),
+      price: Number(this.form.controls['price'].value),
+      created_by: this.form.controls['created_by'].value,
     };
     this.HttpApi.patchRequest(id, body).subscribe((response) => {
       console.log(response);
       // 更新成功後的動作
       this.visible = false;
+      this.getAll();
       Swal.fire({
         icon: 'success',
         title: '成功',
@@ -122,11 +128,13 @@ export class PurchaseComponent {
   }
 
   //刪除
-  delete(id: any): void {
-    this.HttpApi.deleteRequest(id).subscribe((response) => {
+  delete(purchase_id : any): void {
+    console.log(purchase_id)
+    this.HttpApi.deleteRequest(purchase_id).subscribe((response) => {
       console.log(response);
       // 刪除成功後的動作
       this.visible = false;
+      this.getAll();
       Swal.fire({
         icon: 'success',
         title: '成功',
@@ -134,25 +142,16 @@ export class PurchaseComponent {
     });
   }
 
-
-  data: any = [
-    {
-      name: '',
-      position: '',
-      company: '',
-      department: '',
-      product: '',
-      quantity: 0 ,
-      price: 0 ,
-      creater: '',
-    },
-  ];
+  purchase_id : any
 
   showDialog(purchase: any): void {
-    this.data = purchase;
-    console.log('data.position:' + this.data.position);
+    // this.data = purchase;
+    // console.log('data.purchase_id:' + this.data.purchase_id);
     this.visible = true;
+    this.form.patchValue(purchase);
     console.log('purchase:' + JSON.stringify(purchase));
+    this.purchase_id = purchase.purchase_id;
+    console.log(this.purchase_id);
   }
 
   confirm(): void {
@@ -166,4 +165,5 @@ export class PurchaseComponent {
   cancel(): void {
     this.visible = false;
   }
+
 }
